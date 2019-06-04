0cfc84907e8e6e5f877cdbac7dba6b2f6c3b40c6
Jennifer Browning <browningjen@yahoo.com>
2019-06-03 20:43:58 -0400

    changed urls for login and logout

--- twitterclone/authentication/urls.py
+++ twitterclone/authentication/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from twitterclone.authentication.views import (login_view, logout_view)
+from twitterclone.authentication.views import (LoginView, LogoutView)
 
 
 urlpatterns = [
-    path("login/", login_view),
-    path("logout/", logout_view)
+    path("login/", LoginView.as_view()),
+    path("logout/", LogoutView.as_view())
 ]
 
